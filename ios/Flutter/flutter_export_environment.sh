57e166bb2c5dc6583a4ad07e6a761da3054aaa0f
Palash Shrote <palash.shrote.58@gmail.com>
2024-06-25 13:12:32 +0530

    06/24 updated

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\dlfin\AndroidStudioProjects\destini-challenge-starting"
+export "FLUTTER_ROOT=/home/palashshrote58/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/media/palashshrote58/New Volume/trial_nd_trial"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
--- lib/story.dart
+++ lib/story.dart
@@ -1,4 +1,4 @@
 class Story {
   String storyTitle, choice1, choice2;
-  Story({this.storyTitle, this.choice1, this.choice2});
+  Story({required this.storyTitle, required this.choice1, required this.choice2});
 }
